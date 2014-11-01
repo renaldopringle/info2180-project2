2d0432b0d63dfdf4f297467c9d0282c449a6a3b4
Renaldo Pringle <rapchristoff@gmail.com>
2014-11-01 16:28:56 -0500

    update

--- javascript/fifteen.js
+++ javascript/fifteen.js
@@ -1,63 +1,78 @@
 /*WARNING USING PROTOTYPE VERS. <script src="http://ajax.googleapis.com/ajax/libs/prototype/1.7.2.0/prototype.js"></script>*/
 
-var puzzlepiece = $$('#puzzlearea div');
-console.log(puzzlepiece);
+var shufflebutton = document.getElementById("shufflebutton");
 
-function shuffle() {
-    var x = document.getElementById("shufflebutton");
-    x.addEventListener("click", function () {
-        /*for (var i = puzzlepiece.length - 1; i > 0; i--) {
-            var j = Math.floor(Math.random() * (i + 1));
-            var temp = puzzlepiece[i];
-            puzzlepiece[i] = puzzlepiece[j];
-            puzzlepiece[j] = temp;
-        }*/
-        console.log('shuffle');
-    });
-}
-shuffle();
+var puzzlepiece = $$('#puzzlearea div');
+//console.log(puzzlepiece);
+//var piece = document.getElementsByClassName("puzzlepiece");
+
+
 
 $$('#puzzlearea div').each(function (piece) {
     piece.setStyle({
         position: 'relative',
-        'float': 'left'
+        'float': 'left',
+        left: '0px',
+        top: '0px'
     });
     piece.addClassName('puzzlepiece');
 });
-
+var puzzle = document.getElementsByClassName("puzzlepiece");
+for (var i = 0; i < puzzle.length; i++) {
+    puzzle[i].setStyle({backgroundColor: "red"});
+}
 function moveright(piece) {
-    piece.setStyle({'left': '100px'});
+    var i = piece.getStyle('left');
+    i = (i.split("p"));
+    i = (i[0]);
+    i += 100;
+    i = i + "px";
+    piece.setStyle({'left': i});
 }
 function moveleft(piece) {
-    piece.setStyle({left: '-100px'});
+    var i = piece.getStyle('left');
+    i = (i.split("p"));
+    i = (i[0]);
+    i -= 100;
+    i = i + "px";
+    piece.setStyle({'left': i});
 }
 function moveup(piece) {
-    piece.setStyle({top: '-100px'});
+    var i = piece.getStyle('left');
+    i = (i.split("p"));
+    i = (i[0]);
+    i -= 100;
+    i = i + "px";
+    piece.setStyle({'top': i});
 }
 function movedown(piece) {
-    piece.setStyle({top: '100px'});
+    var i = piece.getStyle('left');
+    i = (i.split("p"));
+    i = (i[0]);
+    i += 100;
+    i = i + "px";
+    piece.setStyle({'top': i});
 }
 
-/*$$('.puzzlepiece').each(function(piece) {
-    piece.observe('click', function(event) {
-        moveright(piece);
-    });
-});*/
 
-/*$$('.puzzlepiece').each(function(piece) {
-    piece.addEventListener("click", function() {
-        moveright(piece);
-    });
-});*/
-
-/*$$('.puzzlepiece').each(function(piece) {
-    piece.on('click', function() {
-        moveright(piece);
-    });
-});*/
 $$('.puzzlepiece').each(function (piece) {
     piece.on('click', function () {
         moveleft(piece);
         console.log(piece);
     });
 });
+
+
+function shuffle(x,shufflebutton) {
+    shufflebutton.addEventListener("click", function () {
+        for (var i = x.length - 1; i > 0; i--) {
+            var j = Math.floor(Math.random() * (i + 1));
+            var temp = x[i];
+            x[i] = x[j];
+            x[j] = temp;
+        }
+        console.log(x);
+        return x;
+    });
+}
+puzzle = shuffle(puzzle,shufflebutton);
